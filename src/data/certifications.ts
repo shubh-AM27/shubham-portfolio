e561cd1819e271417dcdf1c6103c9550108e2c62
shubh-AM27 <shubhamsinghedu2004@gmail.com>
2026-05-15 18:10:05 +0530

    Fix spelling of 'certification' in certifications list

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -3,7 +3,7 @@ export const certifications = [
   { name: "Introduction to Cybersecurity", issuer: "Cisco", year: "2026", url: "https://www.credly.com/badges/35f74827-1a6f-45ea-aaa1-260681b98462/public_url" },
   { name: "Fortinet Certified Fundamentals", issuer: "Fortinet", year: "2026", url: "#" },
   { name: "Web Development Fundamentals", issuer: "IBM", year: "2026", url: "#" },
-  { name: "Fortinet NSE 1  certication", issuer: "Fortinet", year: "2026", url: "https://www.credly.com/badges/7426df5c-cfc5-4111-9744-88728e2897b2/public_url" },
-  { name: "Fortinet NSE 2 certication", issuer: "Fortinet", year: "2026", url: "https://www.credly.com/badges/0b7005a3-186a-4065-a3da-82da47073d3c/public_url" }
+  { name: "Fortinet NSE 1  certification", issuer: "Fortinet", year: "2026", url: "https://www.credly.com/badges/7426df5c-cfc5-4111-9744-88728e2897b2/public_url" },
+  { name: "Fortinet NSE 2 certification", issuer: "Fortinet", year: "2026", url: "https://www.credly.com/badges/0b7005a3-186a-4065-a3da-82da47073d3c/public_url" }
   
 ];
